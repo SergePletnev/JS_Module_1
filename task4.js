0b4bbba7895b76e3ab2a31493a770a1516e37581
Siarhei Plistniou <siarhei_pliatniou@epam.com>
2018-06-20 17:08:22 +0300

    Revert "fix"
    This reverts commit 8e1adc4d20a1b6ae8496b450d5f1e87ceb29ab0d.

--- task4.js
+++ task4.js
@@ -1,5 +1,3 @@
-'use strict';
-
 function getCountryInformationCallbackNodeRestClient(countryCode, callback) {
     const Client = require('node-rest-client').Client;
     const client = new Client();
